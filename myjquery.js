f43324d25afb138ec21c8f53acdcc74e8fd82536
Naomi K <naomi@multidots.com>
2022-12-30 14:45:39 -0500

    test run for locking landscape orientation

--- myjquery.js
+++ myjquery.js
@@ -1,8 +1,26 @@
 $(function(){
+	// Hide game table on start menu
 	$('#divTable').css('opacity', 0);
+	// Show game table and hide title
 	$('.start-btn').on('click', () => {
 		console.log($('#title'))
 		$('#title').css('opacity', 0);
 		$('#divTable').css('opacity', 1);
-	})
+	});
+	$(window).resize( () => {
+		let width = $(window).width();
+		let triggerWidth = 790;
+		console.log(width);
+		if (width <= triggerWidth) {
+			alert('If on mobile device, it is recommended to play horizontally.');
+		}
+	});
 });
+
+screen.orientation.lock("landscape")
+	.then(function() {
+		alert('Locked');
+	})
+	.catch(function(error) {
+		alert(error);
+	});
