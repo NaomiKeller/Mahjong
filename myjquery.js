1e87263b1a466f35c629a4229deaaff53a5bf3b6
Naomi K <naomi@multidots.com>
2022-12-30 14:00:57 -0500

    more responsive testing >_<

--- myjquery.js
+++ myjquery.js
@@ -1,6 +1,8 @@
 $(function(){
+	$('#divTable').css('opacity', 0);
 	$('.start-btn').on('click', () => {
 		console.log($('#title'))
 		$('#title').css('opacity', 0);
+		$('#divTable').css('opacity', 1);
 	})
 });
