bc10ee91a7d60d61c2c631f061673f991a443baf
Naomi K <naomi@multidots.com>
2023-01-02 13:12:48 -0500

    help modal added

--- myjquery.js
+++ myjquery.js
@@ -1,4 +1,6 @@
 $(function(){
+	// Hide overflow on start menu
+	$('body').css('overflow', 'hidden');
 	// Hide game table on start menu
 	$('#divTable').css('opacity', 0);
 	// Show game table and hide title
@@ -29,10 +31,26 @@ $(function(){
 		$('.orientation-warning').css('display', 'none');
 	});
 	$('.start-btn').on('click', () => {
+		$('body').css('overflow', 'auto');
 		let width = $('#divTable').width();
 		if ( width > 681 ) {
 			$('.orientation-warning').css('font-size', '120%');
 			$('.orientation-warning').find('span').css('font-size', '120%');
 		}
+	});
+	// help modal
+	$('.help-icon').on('click', () => {
+		$('body').css('overflow', 'auto');
+		$('.dim-background').css('visibility', 'visible');
+		$('.div-help').css('visibility', 'visible');
+		$('.dim-background').css('opacity', 1);
+		$('.div-help').css('opacity', 1);
+	});
+	$('.help-exit').on('click', () => {
+		$('body').css('overflow', 'hidden');
+		$('.dim-background').css('visibility', 'hidden');
+		$('.div-help').css('visibility', 'hidden');
+		$('.dim-background').css('opacity', 0);
+		$('.div-help').css('opacity', 0);
 	});
 });
